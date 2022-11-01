b8a1507c5e552f6167546e3bdd88cb555c6bf213
moonix0 <moongraph21@gmail.com>
2022-11-01 13:30:38 +0700

    UI/TailwindCSS Simple Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import Features from './components/Features';
 const App = () => {
   return (
     <section className='h-screen w-screen bg-slate-800'>
       <div className='flex items-center justify-center h-screen'>
-        <h1 className='text-6xl text-lime-400'>
+        <h1 className='text-6xl text-lime-400 font-primary'>
           Duid<span>Trackr.</span>
         </h1>
       </div>
