3d903401195fbc8d835b322b4c9f309b9ffb850c
moonix0 <moongraph21@gmail.com>
2022-11-01 12:20:24 +0700

    Initialize Project Using Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Features from './components/Features';
+const App = () => {
+  return (
+    <section className='h-screen w-screen bg-slate-800'>
+      <div className='flex items-center justify-center h-screen'>
+        <h1 className='text-6xl text-lime-400'>
+          Duid<span>Trackr.</span>
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default App;
